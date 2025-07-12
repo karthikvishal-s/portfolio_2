f1259010e7956dd65f92d7fd1dfb0782034841b7
Karthik Vishal <karthikvishal1506@gmail.com>
2025-07-12 22:51:55 +0530

    added stack icons in projectcards

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -7,17 +7,30 @@ type ProjectCardProps = {
     title: string;
     description: string;
     pic:{src:string};
+    stacks: string[];
   };
   
-  const ProjectCard = ({ title, description,pic }: ProjectCardProps) => {
+  const ProjectCard = ({ title, description,pic,stacks }: ProjectCardProps) => {
     return (
-      <div className="bg-black hover:scale-110 border border-gray-400 rounded-4xl p-4  mb-4 w-full max-w-xl transition delay-50 duration-400 hover:bg-[#222224]">
+      <div className="bg-black hover:scale-110 border border-gray-700 rounded-4xl p-4  mb-4 w-full max-w-xl transition delay-50 duration-400 hover:bg-[#222224]">
         <h2 className="text-xl font-semibold text-white text-center">{title}</h2>
         <img src={pic.src} alt="Project Thumbnail" className="w-full h-48 object-cover rounded-full mt-2" />
-        <p className="text-gray-400 mt-2">{description}</p>
+
+        <div>
+          <div className="flex text-center justify-center items-center flex-wrap mt-4">
+          
+            {stacks.map((stack,index)=>
+            <div>
+              <span key={index} className="inline-block bg-[#400e69] text-center text-white text-[9px] font-raleway px-2 py-1 rounded mr-2 mt-2">{stack}</span>
+            </div>
+            )}
+          </div>
+        </div>
+
+        <p className="text-white mt-4 text-center font-raleway text-xs">{description}</p>
         <div>
         <div className="flex text-center justify-center items-center mt-4 space-x-4">
-            <Link to={"http://google.com"} className="hover:scale-110 transition delay-50 duration-400 hover:text-blue-500"><button className="flex px-3 py-2 rounded-full mt-2 ">
+            <Link to={"http://google.com"} className="hover:scale-110 transition delay-50 duration-400 hover:text-blue-500"><button className="flex px-3 py-2 rounded-full mt2 ">
                 
             <Globe className="w-5 h-5 relative -bottom-0 text-blue-500" />  <p className="ml-2 relative -top-0">Live</p>  <ChevronRight className="w-6 h-6 relative"/> </button>
                 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,44 +6,75 @@ const defaultPic = { src: kv };
 
 const ProjectData = [
   {
-    title: "Project 1",
-    description: "This is the first project.",
-    pic: { src: kv }
+    title: "Noire",
+    description: "An E-commerce marketplace designed to empower small businesses and independant designers.",
+    pic: { src: kv },
+    stacks: ["NextJS","ReactJS","TailwindCSS", "NodeJS", "ExpressJS", "MongoDB","Stripe"]
   },
   {
-    title: "Project 2",
-    description: "This is the second project."
+    title: "The Raven's Roost",
+    description: "A social media platform for GOT fans to connect, share, and discuss their favorite series.",
+    stacks: ["NextJS", "ReactJS", "TailwindCSS","NextAuth", "NodeJS", "ExpressJS", "MongoDB","Vercel","Google OAuth" ],
   },
   {
-    title: "Project 3",
-    description: "This is the third project."
+    title: "Pensieve",
+    description: "A Public Book Repository for Enthusiasts.Designed to be community-driven, this platform enables users to contribute, discover, and interact with curated book collections.",
+    stacks: ["ReactJS", "TailwindCSS", "NodeJS", "ExpressJS", "MongoDB"]
   },
   {
-    title: "Project 4",
-    description: "This is the fourth project."
+    title: "DocHelp - in Progress",
+    description: "(Still in Progress)   Designed to help doctors efficiently manage patient data in an orthopedic clinic.",
+    stacks: ["VueJs", "TailwindCSS", "NodeJS", "ExpressJS", "PostgreSQL"]
+
   },
   {
-    title: "Project 5",
-    description: "This is the fifth project."
+    title: "UnitED",
+    description: "Created for the Mega Hackathon 2025 under the theme of SDE Goals, this app focuses on Goal 4 (Education), providing a safe space for students dealing with anxiety and introversion.",
+    stacks:["ReactJS", "TailwindCSS","Firebase", "FireStore DB"]
   },
   {
-    title: "Project 6",
-    description: "This is the sixth project."
+    title: "QR Generator",
+    description: "Built using Node and Express, this QR code generator allows users to create custom QR codes for various purposes.",
+    stacks: ["NodeJS", "ExpressJS", "TailwindCSS", "MongoDB"]
+  },
+  
+  {
+    title:"SportsHub",
+    description:"A FrontEnd Application for Sports Enthusiasts, providing a platform to explore and engage with various sports-related content.",
+    stacks: ["HTML", "CSS", "Javascript"]
+  },
+  {
+    title: "Portfolio - 1",
+    description: "Personal portfolio website, showcasing various animations and transformations. Enhanced with multiple React extensions for better interactivity.",
+    stacks: ["ReactJS", "TailwindCSS", "Framer Motion", "React Router", "lucide-react"]
+
+  },
+  
+  {
+    title: "Portfolio - 2",
+    description: "Personal portfolio website, showcasing various animations and transformations. Enhanced with multiple React extensions for better interactivity.",
+    stacks: ["ReactJS", "TailwindCSS", "Framer Motion", "React Router", "lucide-react","MaterialUI"]
+
   }
+  
 ];
 
 const Projects = () => {
   return (
     <div className="min-h-screen min-w-screen flex flex-col">
       <Navbar sl={2} />
+      <p className='text-center font-raleway text-4xl mt-10 mb-10'>Projects</p>
       <div className="flex-grow p-8 flex justify-center items-start">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-16 w-full max-w-2xl">
+        
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-16 w-full max-w-6xl">
           {ProjectData.map((project, index) => (
             <ProjectCard
               key={index}
               title={project.title}
               description={project.description}
               pic={project.pic ?? defaultPic} // Safe fallback
+              stacks={project.stacks || []} // Default to empty array if stacks are not provided
+          
             />
           ))}
         </div>
